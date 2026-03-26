38b6f3cf8a2b80a3194eabc125791fc87786d5c8
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-03-26 23:56:32 +0530

    Added pattern program

---
+++ patterns/PatternRightStarTriangle.java
@@ -0,0 +1,20 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class PatternRightStarTriangle {
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows : ");
+        int n = new Scanner(System.in).nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
