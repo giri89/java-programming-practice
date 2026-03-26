0bc5228c618e0ce52346ffcb63c8df3ea5a8743c
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-03-26 23:55:58 +0530

    Added pattern program

---
+++ patterns/PatternRightSameNumber.java
@@ -0,0 +1,20 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class PatternRightSameNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows : ");
+        int n = new Scanner(System.in).nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
