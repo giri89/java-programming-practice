55fe22f0ecf9ed3f1f429f9c9cd017c70d58f191
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-03-28 23:17:14 +0530

    Started array programs

---
+++ arrays/CreateArray.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class CreateArray {
+    public static void main(String[] args) {
+        int[] a = {10, 12, 25, 8, 12};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
