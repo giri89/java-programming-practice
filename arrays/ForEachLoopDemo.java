426596e6451e6a3677593cdf3ea427c540d7cc6e
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-03-29 23:21:22 +0530

    Added array programs

---
+++ arrays/ForEachLoopDemo.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class ForEachLoopDemo {
+    public static void main(String[] args) {
+        int[] a = {9, 2, 8, 6, 7};
+
+        for (int x : a) {
+            System.out.println(x);
+        }
+    }
+}
