5f5c870ff67207f35c113e632ec9bdd98cbcc2ee
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-04-02 23:49:44 +0530

    Added number programs

---
+++ numbers/ArmstrongNumber.java
@@ -0,0 +1,38 @@
+package numbers;
+
+public class ArmstrongNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isArmStrong(153));
+    }
+
+    static boolean isArmStrong(int n) {
+        int digitCount = numberOfDigit(n);
+        int temp = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            sum = sum + power(rem, digitCount);
+            n = n / 10;
+        }
+        return temp == sum;
+    }
+
+    static int numberOfDigit(int n) {
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    static int power(int x, int y) {
+        int res = 1;
+        for (int i = 1; i <= y; i++) {
+            res = res * x;
+        }
+        return res;
+    }
+}
