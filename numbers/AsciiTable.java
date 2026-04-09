d2d595fc27490417d336c2aca88f3c36d23651ae
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-04-09 12:15:49 +0530

    added a AsciiTable number program

---
+++ numbers/AsciiTable.java
@@ -0,0 +1,9 @@
+package numbers;
+
+public class AsciiTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 130; i++) {
+            System.out.println(i + " " + (char) (i));
+        }
+    }
+}
