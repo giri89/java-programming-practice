b8f4163adad27ae7c0eb1eb91b03bb41ea7111eb
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-04-30 21:59:38 +0530

    practiced an array program

---
+++ arrays/SortingInbuilt.java
@@ -0,0 +1,12 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class SortingInbuilt {
+
+    public static void main(String[] args) {
+        int[] a = {9, 5, 4, 1, 10, 6, 3, 2, 8, 1, 2, 3, 0};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
