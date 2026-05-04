f747a44a565d34024e55e5d3bc4a382f8c858af8
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-05-04 22:18:29 +0530

    added a pascaltriangle program

---
+++ arrays/PascalTriangleFull.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class PascalTriangleFull {
+    public static void main(String[] args) {
+        int n = 5;
+        int a[][] = new int[n][];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new int[i + 1];
+            for (int j = 0; j < a[i].length; j++) {
+                if (j == 0 || j == a[i].length - 1) {
+                    a[i][j] = 1;
+                } else {
+                    a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+                }
+            }
+        }
+        for (int x[] : a) {
+            System.out.println(Arrays.toString(x));
+        }
+    }
+}
