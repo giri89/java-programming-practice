a27895d969ab901f82cddf478657c3844737358c
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-05-06 22:57:42 +0530

    Added a sorting program

---
+++ arrays/BubbleSortOptimized.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class BubbleSortOptimized {
+
+    public static void main(String[] args) {
+        int[] a = {9, 2, 4, 1, 10, 6, 3, 2, 8, 1, 2, 3};
+        solution(a);
+    }
+
+    static void solution(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
