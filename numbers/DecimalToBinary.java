25f64d110d397f50e4be7e281c9d425b96c14165
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-05-17 22:24:45 +0530

    Add a number program

---
+++ numbers/DecimalToBinary.java
@@ -0,0 +1,23 @@
+package numbers;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        decimalToBinary(13);
+    }
+
+    static void decimalToBinary(int n) {
+
+        String binary = "";
+
+        while (n > 0) {
+
+            int rem = n % 2;
+
+            binary = rem + binary;
+
+            n = n / 2;
+        }
+
+        System.out.println(binary);
+    }
+}
