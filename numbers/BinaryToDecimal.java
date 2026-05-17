4098d5669054d9bc3715ac28c4c340f80676f39f
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-05-17 22:24:03 +0530

    Add a number program

---
+++ numbers/BinaryToDecimal.java
@@ -0,0 +1,26 @@
+package numbers;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        System.out.println(binaryToDecimal(1011));
+    }
+
+    static int binaryToDecimal(int n) {
+
+        int sum = 0;
+        int power = 0;
+
+        while (n > 0) {
+
+            int rem = n % 10;
+
+            sum = sum + rem * (int)Math.pow(2, power);
+
+            power++;
+
+            n = n / 10;
+        }
+
+        return sum;
+    }
+}
