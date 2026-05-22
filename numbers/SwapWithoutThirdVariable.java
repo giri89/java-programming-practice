cbfc27d68102499bd8e88234510f9b4dcad7e054
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-05-22 23:14:36 +0530

    Add a numbers program

---
+++ numbers/SwapWithoutThirdVariable.java
@@ -0,0 +1,21 @@
+package numbers;
+
+public class SwapWithoutThirdVariable {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before Swap");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swap");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
