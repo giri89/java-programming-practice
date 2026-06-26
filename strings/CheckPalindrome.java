1657c2dbac3535b79b0ded34fb8e3d5c1247cd75
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-06-26 22:57:43 +0530

    Add a string program

---
+++ strings/CheckPalindrome.java
@@ -0,0 +1,28 @@
+package strings;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+
+        String str = "MADAM";
+
+        System.out.println(isPalindrome(str));
+    }
+
+    static boolean isPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
