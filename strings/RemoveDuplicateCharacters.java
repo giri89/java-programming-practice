0a4882362f5ad7503176a4368fe3626ee8b55a12
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-06-27 23:04:28 +0530

    Add a string program

---
+++ strings/RemoveDuplicateCharacters.java
@@ -0,0 +1,26 @@
+package strings;
+
+public class RemoveDuplicateCharacters {
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        System.out.println(removeDuplicates(str));
+    }
+
+    static String removeDuplicates(String str) {
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (result.indexOf(ch) == -1) {
+                result += ch;
+            }
+        }
+
+        return result;
+    }
+}
