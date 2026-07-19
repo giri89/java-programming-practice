b60abb3829d64a5a8cbc2ce44d721995b477d417
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-07-19 23:57:23 +0530

    Add a string program

---
+++ strings/EndsWithCheck.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class EndsWithCheck {
+    public static void main(String[] args) {
+
+        String str = "Programming.java";
+
+        System.out.println(str.endsWith(".java"));
+    }
+}
