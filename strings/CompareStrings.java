f7286fbea7a90520daeaa922ffaf5fc3555a53aa
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-07-21 22:44:48 +0530

    Add a string program

---
+++ strings/CompareStrings.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+
+        String s1 = "Java";
+        String s2 = "Java";
+
+        System.out.println(s1.equals(s2));
+    }
+}
