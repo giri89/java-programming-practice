cf45f46f2f040eeb2b6f179b3980939cb7c2250d
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-07-24 23:50:41 +0530

    Add a array program

---
+++ arrays/LeftRotateArray.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class LeftRotateArray {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        leftRotate(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void leftRotate(int[] arr) {
+
+        int first = arr[0];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[arr.length - 1] = first;
+    }
+}
