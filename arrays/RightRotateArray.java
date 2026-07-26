b2b7cb9b18b6a1460190f057664c9724dfff8da0
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-07-26 22:33:14 +0530

    Add a array program

---
+++ arrays/RightRotateArray.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class RightRotateArray {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        rightRotate(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void rightRotate(int[] arr) {
+
+        int last = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = last;
+    }
+}
