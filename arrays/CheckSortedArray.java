c7fc17a62d9722fda34e2464e5e3ef25d0c7162c
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-07-27 23:39:08 +0530

    Add a array program

---
+++ arrays/CheckSortedArray.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class CheckSortedArray {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println(isSorted(arr));
+    }
+
+    static boolean isSorted(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
